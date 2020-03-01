3635f110d670c78d7bdf22815b5642299fb8df5c
mhexsel <marcelo.hexsel@hotmail.com>
2020-02-29 23:46:11 -0300

    node-project

---
+++ node-project/index.js
@@ -0,0 +1,13 @@
+console.log("hello world nodejs console");
+
+var counter = 0;
+
+counter++;
+
+console.log("Counter:", counter);
+
+var minhaFuncao = function() {
+  console.log("executei a minhaFuncao");
+};
+
+minhaFuncao();
